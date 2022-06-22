eae274e6cd51dbd6691bbc514063375833d5feae
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-06-22 14:36:20 -0500

    fixed something
    it didn't allow the test to work

--- 0/messages.js
+++ 0/messages.js
@@ -2,7 +2,7 @@ function test()
 {
   let r = new XMLHttRequest();
   r.addEventListener("load", function(){
-    document.getElementById("test").innerHTML = this.responseText;
+    document.getElementById("result").innerHTML = this.responseText;
   });
   r.open("GET", "./messages.js");
   r.send();
