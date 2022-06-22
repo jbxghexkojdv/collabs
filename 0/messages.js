4991bc6760d800c83e9817d13d87f21f88d2929d
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-06-22 14:19:09 -0500

    response text

--- 0/messages.js
+++ 0/messages.js
@@ -2,7 +2,7 @@ function test()
 {
   let r = new XMLHttRequest();
   r.addEventListener("load", function(){
-    document.getElementById("test").innerHTML = 0;
+    document.getElementById("test").innerHTML = this.responseText;
   });
   r.open("GET", "./messages.js");
   r.send();
