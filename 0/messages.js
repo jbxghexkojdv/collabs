cca51fcd02942ae6411d26a1784eb6ad72903ded
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-06-22 17:32:24 -0500

    no mpre test pt 1

--- 0/messages.js
+++ 0/messages.js
@@ -1,12 +1,1 @@
-function amogd()
-{
-  document.getElementById("result").innerHTML = this.status;
-}
 
-function test()
-{
-  let r = new XMLHttpRequest();
-  r.addEventListener("load", amogd);
-  r.open("GET", "./messages.js");
-  r.send();
-}
