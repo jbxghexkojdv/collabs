2a798c2989367895ad341f817744aadc26157b4f
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-06-22 14:17:36 -0500

    added the request

--- 0/messages.js
+++ 0/messages.js
@@ -1,4 +1,9 @@
 function test()
 {
   let r = new XMLHttRequest();
+  r.addEventListener("load", function(){
+    document.getElementById("test").innerHTML = 0;
+  });
+  r.open("GET", "./messages.js");
+  r.send();
 }
