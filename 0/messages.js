d8c29495e08db9d0065a57530b9e7e9e91ee05af
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-06-22 21:10:49 -0500

    basic stuff
    Added some initial classes and imported the json

--- 0/messages.js
+++ 0/messages.js
@@ -1,1 +1,25 @@
+import chatData from './chat.json' assert {type: 'json'};
 
+function User(name = "Anonymous Dab", username = Math.random(), pfpLink = "", desc = "", email = "noreply@youtube.com")
+{
+  this.name = name;
+  this.pfp = pfpLink;
+  this.profile = desc;
+  this.email = email;
+  this.friends = [];
+  this.isDev = false;
+}
+
+function Message(userId, text = "I somehow sent no message:)", date = new Date("Apr 20, 1969 13:37"))
+{
+  this.user = userId;
+  this.text = text;
+  this.time = date;
+}
+
+function Chat(ownerId)
+{
+  this.owner = ownerId;
+  this.messages = [];
+  this.people = [ownerId];
+}
